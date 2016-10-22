f48d09b980861c5aea90ad540cf729cc50440392
Robert Coffey <racoffey@gmail.com>
2016-10-22 20:59:44 +0200

    Minor updates

--- ImageAnalyserUITests/ImageAnalyserUITests.swift
+++ ImageAnalyserUITests/ImageAnalyserUITests.swift
@@ -11,7 +11,7 @@ import XCTest
 class ImageAnalyserUITests: XCTestCase {
         
     override func setUp() {
-        super.setUp()
+        super.setUp()    
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
